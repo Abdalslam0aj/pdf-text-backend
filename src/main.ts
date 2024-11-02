d57fddc4deaffd21eafa6474c30094e398198be9
Abdalslam0aj <abodajaljmal@gmail.com>
2024-11-02 18:44:37 +0300

    Added cors so i can test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //i will only use this for this demo :)
   app.enableCors();
   await app.listen(process.env.PORT ?? 3001);
 }
